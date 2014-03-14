a8441488c5f63e7284839dc797f32425976165be
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-14 23:30:02 +0100

    * add dev karma configuration file

---
+++ test/dev.karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+    'use strict';
+
+    config.set({
+        basePath: '..',
+        frameworks: ['jasmine', 'requirejs'],
+
+        files: [
+            {pattern: 'lib/**/*.js', included: false},
+            {pattern: 'test/specs/**/*-spec.js', included: false},
+
+            'test/test-main.js'
+        ],
+
+        browsers: ['PhantomJS'],
+        reporters: ['dots', 'coverage'],
+        preprocessors: {
+            'lib/**/*.js': ['coverage']
+        },
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage'
+        },
+
+        logLevel: config.LOG_INFO,
+
+        autoWatch: true,
+        singleRun: false
+    });
+};
