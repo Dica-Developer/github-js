69cfb7caf8164c1760a4d0eb4f5963adc9a1c072
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-21 20:43:58 +0100

    * update events spec

--- test/specs/events-spec.js
+++ test/specs/events-spec.js
@@ -62,7 +62,7 @@ define(['githubjs'], function (Client) {
         it('should successfully execute GET /repos/:user/:repo/issues/events (getFromRepoIssues)', function (done) {
             function callback(err, res) {
                 expect(err).toBeNull();
-                expect(res.length).toBe(1);
+                expect(res.length).toBe(2);
                 var last = res.pop();
                 expect(last.event).toBe('closed');
                 expect(last.created_at).toBe('2014-03-15T00:40:10Z');
