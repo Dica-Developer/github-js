d67130650d61234d4a733f9184567b7b4636c6fa
Jörg Weber <weber.joerg.h@gmail.com>
2014-09-24 11:38:21 +0200

    * add user specs

--- test/specs/user-spec.js
+++ test/specs/user-spec.js
@@ -4,10 +4,11 @@
     describe('user', function () {
         var github;
         var token = '44046cd4b4b85afebfe3ccaec13fd8c08cc80aad';
+        var sshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAm8TLnlXY+M+FtTEV4crIXpB/AX25jBODpUkIFFokGvIdU1sI4USTwDYX1vOMTQ9o8PFKgj+L2jeTkj0B+lh8Gnl8MF60eMu1Zfadedba6ENF8Ci01EFwtQIFMhKHjgj3f5LNyeDJXV5HAP+JVdwfvk+US+wbjUnBfCcGrr1C7wXB1yeGwd0YV5XSG9jOOt0g9L/DoZQ9XIzckpMed0YrR7rYFqxVoRMkkpsPOZyYLj5bmncZw8J5dD+8g8RPB/viRpvk6xPODbp/hW8A64qb4lJZDYevufyzAGI+pj9U1EAEYjmeJbn6eJbPveL+/jIT/xylcDoPGq0a6lE6L+JyXQ== horus381@gmx.de';
 
         beforeEach(function () {
             github = new Github();
-            client.authenticate({
+            github.authenticate({
                 type: 'oauth',
                 token: token
             });
@@ -16,12 +17,13 @@
         it('should successfully execute GET /users/:user (getFrom)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.login).toBe('JayGray');
                 done();
             };
 
             github.user.getFrom(
                 {
-                    user: 'String'
+                    user: 'JayGray'
                 },
                 callback
             );
@@ -30,6 +32,7 @@
         it('should successfully execute GET /user (get)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.login).toBe('jwebertest');
                 done();
             };
 
@@ -39,7 +42,7 @@
             );
         });
 
-        it('should successfully execute PATCH /user (update)', function (done) {
+        xit('should successfully execute PATCH /user (update)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
                 done();
@@ -62,14 +65,11 @@
         it('should successfully execute GET /user/orgs (getOrgs)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBe(0);
                 done();
             };
 
-            github.user.getOrgs(
-                {
-                    page: 'Number',
-                    per_page: 'Number'
-                },
+            github.user.getOrgs({},
                 callback
             );
         });
@@ -77,14 +77,11 @@
         it('should successfully execute GET /user/teams (getTeams)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBe(0);
                 done();
             };
 
-            github.user.getTeams(
-                {
-                    page: 'Number',
-                    per_page: 'Number'
-                },
+            github.user.getTeams({},
                 callback
             );
         });
@@ -92,31 +89,38 @@
         it('should successfully execute GET /user/emails (getEmails)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBe(1);
+                expect(res[0].verified).toBe(true);
                 done();
             };
 
-            github.user.getEmails(
+            github.user.getEmails({},
+                callback
+            );
+        });
+
+        xit('should successfully execute POST /user/emails (addEmails)', function (done) {
+            var callback = function (err, res) {
+                expect(err).toBeNull();
+                github.user.deleteEmails(
+                    {
+                        'email': ['info@dica-developer.org']
+                    }, function(err, res){
+                        expect(err).toBeNull();
+                        done();
+                    }
+                );
+            };
+
+            github.user.addEmails(
                 {
-                    page: 'Number',
-                    per_page: 'Number'
+                    email: ['info@dica-developer.org']
                 },
                 callback
             );
         });
 
-        it('should successfully execute POST /user/emails (addEmails)', function (done) {
-            var callback = function (err, res) {
-                expect(err).toBeNull();
-                done();
-            };
-
-            github.user.addEmails(
-                {},
-                callback
-            );
-        });
-
-        it('should successfully execute DELETE /user/emails (deleteEmails)', function (done) {
+        xit('should successfully execute DELETE /user/emails (deleteEmails)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
                 done();
@@ -131,14 +135,13 @@
         it('should successfully execute GET /users/:user/followers (getFollowers)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBeGreaterThan(0);
                 done();
             };
 
             github.user.getFollowers(
                 {
-                    user: 'String',
-                    page: 'Number',
-                    per_page: 'Number'
+                    user: 'JayGray'
                 },
                 callback
             );
@@ -147,14 +150,13 @@
         it('should successfully execute GET /users/:user/following (getFollowingFromUser)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBeGreaterThan(0);
                 done();
             };
 
             github.user.getFollowingFromUser(
                 {
-                    user: 'String',
-                    page: 'Number',
-                    per_page: 'Number'
+                    user: 'JayGray'
                 },
                 callback
             );
@@ -166,11 +168,7 @@
                 done();
             };
 
-            github.user.getFollowing(
-                {
-                    page: 'Number',
-                    per_page: 'Number'
-                },
+            github.user.getFollowing({},
                 callback
             );
         });
@@ -183,9 +181,7 @@
 
             github.user.getFollowUser(
                 {
-                    user: 'String',
-                    page: 'Number',
-                    per_page: 'Number'
+                    user: 'JayGray'
                 },
                 callback
             );
@@ -194,12 +190,35 @@
         it('should successfully execute PUT /user/following/:user (followUser)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
-                done();
+                github.user.getFollowUser(
+                    {
+                        user: 'mschaaf'
+                    },
+                    function(err, res){
+                        expect(err).toBeNull();
+                        github.user.unFollowUser(
+                            {
+                                user: 'mschaaf'
+                            },
+                            function(err, res){
+                                expect(err).toBeNull();
+                                github.user.getFollowUser(
+                                    {
+                                        user: 'mschaaf'
+                                    },
+                                    function(err, res){
+                                        expect(err).not.toBeNull();
+                                        done();
+                                    })
+                            }
+                        );
+                    }
+                );
             };
 
             github.user.followUser(
                 {
-                    user: 'String'
+                    user: 'mschaaf'
                 },
                 callback
             );
@@ -208,12 +227,35 @@
         it('should successfully execute DELETE /user/following/:user (unFollowUser)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
-                done();
+                github.user.getFollowUser(
+                    {
+                        user: 'mschaaf'
+                    },
+                    function(err, res){
+                        expect(err).toBeNull();
+                        github.user.unFollowUser(
+                            {
+                                user: 'mschaaf'
+                            },
+                            function(err, res){
+                                expect(err).toBeNull();
+                                github.user.getFollowUser(
+                                    {
+                                        user: 'mschaaf'
+                                    },
+                                    function(err, res){
+                                        expect(err).not.toBeNull();
+                                        done();
+                                    })
+                            }
+                        );
+                    }
+                );
             };
 
-            github.user.unFollowUser(
+            github.user.followUser(
                 {
-                    user: 'String'
+                    user: 'mschaaf'
                 },
                 callback
             );
@@ -225,11 +267,7 @@
                 done();
             };
 
-            github.user.getKeys(
-                {
-                    page: 'Number',
-                    per_page: 'Number'
-                },
+            github.user.getKeys({},
                 callback
             );
         });
@@ -237,28 +275,46 @@
         it('should successfully execute GET /users/:user/keys (getKeysFromUser)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
+                expect(res.length).toBeGreaterThan(0);
                 done();
             };
 
             github.user.getKeysFromUser(
                 {
-                    user: 'String',
-                    page: 'Number',
-                    per_page: 'Number'
+                    user: 'JayGray'
                 },
                 callback
             );
         });
 
         it('should successfully execute GET /user/keys/:id (getKey)', function (done) {
+
             var callback = function (err, res) {
                 expect(err).toBeNull();
-                done();
+                github.user.getKey(
+                    {
+                        id: res.id
+                    },
+                    function(err, res){
+                        expect(err).toBeNull();
+                        github.user.deleteKey(
+                            {
+                                id: res.id
+                            },
+                            function(err, res){
+                                expect(err).toBeNull();
+                                done();
+                            }
+                        );
+                    }
+                );
             };
 
-            github.user.getKey(
+
+            github.user.createKey(
                 {
-                    id: 'String'
+                    title: 'Test Key',
+                    key: sshKey
                 },
                 callback
             );
@@ -267,19 +323,27 @@
         it('should successfully execute POST /user/keys (createKey)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
-                done();
+                github.user.deleteKey(
+                    {
+                        id: res.id
+                    },
+                    function(err, res){
+                        expect(err).toBeNull();
+                        done();
+                    }
+                );
             };
 
             github.user.createKey(
                 {
-                    title: 'String',
-                    key: 'String'
+                    title: 'Test Key',
+                    key: sshKey
                 },
                 callback
             );
         });
 
-        it('should successfully execute PATCH /user/keys/:id (updateKey)', function (done) {
+        xit('should successfully execute PATCH /user/keys/:id (updateKey)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
                 done();
@@ -298,12 +362,21 @@
         it('should successfully execute DELETE /user/keys/:id (deleteKey)', function (done) {
             var callback = function (err, res) {
                 expect(err).toBeNull();
-                done();
+                github.user.deleteKey(
+                    {
+                        id: res.id
+                    },
+                    function(err, res){
+                        expect(err).toBeNull();
+                        done();
+                    }
+                );
             };
 
-            github.user.deleteKey(
+            github.user.createKey(
                 {
-                    id: 'String'
+                    title: 'Test Key',
+                    key: sshKey
                 },
                 callback
             );
